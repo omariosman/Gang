1b8c84d738173611a7a65afd15896cc70610b0fb
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-07 00:31:35 +0200

    Update a20_gate.asm

--- sources/includes/second_stage/a20_gate.asm
+++ sources/includes/second_stage/a20_gate.asm
@@ -2,6 +2,11 @@ check_a20_gate:
     pusha                                   ; Save all general purpose registers on the stack
 
             ; This function need to be written by you.
+    mov ax,0x2402
+    int 0x15
+    jc .error
+    cmp al,0x0
+    je .enable_a20
 
     popa                                ; Restore all general purpose registers from the stack
     ret
