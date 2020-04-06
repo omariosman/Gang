8089a48c580bf0a34c2068fb036ce559d13410e4
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-07 00:35:11 +0200

    Update memory_scanner.asm

--- sources/includes/second_stage/memory_scanner.asm
+++ sources/includes/second_stage/memory_scanner.asm
@@ -4,8 +4,30 @@
 %define MEM_MAGIC_NUMBER            0x0534D4150                
     memory_scanner:
             pusha                                       ; Save all general purpose registers on the stack
-
                   ; This function need to be written by you.
+            pusha ; Save all general purpose registers on the stack
+            mov ax,MEM_REGIONS_SEGMENT ; Set ES to 0x2000
+            mov es,ax
+            xor ebx,ebx ; Set EBX to zero
+            mov [es:PTR_MEM_REGIONS_COUNT],word 0x0 ; Use the word at address 0x2000:0x0000 as a counter to count memory regions
+            mov di, PTR_MEM_REGIONS_TABLE ; Set DI to point to address 0x18 to be 24-bytes aligned to store the memory regions table
+            .memory_scanner_loop: ; Loop over available memory regions
+            mov edx,MEM_MAGIC_NUMBER ; Set DX to magic number 0x0534D4150 = 'SMAP'
+            mov word [es:di+20], 0x1 ; This is needed by function 0xe820 int 0x15
+            mov eax, 0xE820 ; Set the memory scanner function number
+            mov ecx,0x18 ; Set size of the memory buffer to store region data
+            int 0x15
+            jc .memory_scan_failed ; If carry flag thens something wrong happened so we need to exit with error message
+            cmp eax,MEM_MAGIC_NUMBER ; If eax is equal to the magic number then everything is okay
+            jnz .memory_scan_failed ; Else something wrong happened so we need to exit with error message
+            add di,0x18 ; Advance di 24 bytes to point to the next entry in the memory region table
+            inc word [es:PTR_MEM_REGIONS_COUNT] ; Increment the memory regions counter
+            cmp ebx,0x0 ; If ebx is 0x0 then no more regions and we need to exit the loop
+            jne .memory_scanner_loop ; Else loop to fetch next region
+            jmp .finish_memory_scan ; If we are here then everything went well so we need to skip the error section
+            .memory_scan_failed:
+            …………..
+            .finish_memory_scan:
 
             popa                                        ; Restore all general purpose registers from the stack
             ret
