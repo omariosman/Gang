5eaffb0d2e9db2bec893995d0b5df6c3cb9e3939
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 17:32:27 +0200

    Update a20_gate.asm

--- sources/includes/second_stage/a20_gate.asm
+++ sources/includes/second_stage/a20_gate.asm
@@ -1,12 +1,12 @@
 check_a20_gate:
-    pusha                                   ; Save all general purpose registers on the stack
+    pusha ; saving all the general purpose registors in the stack again
 
-            ; This function need to be written by you.
-    mov ax,0x2402
-    int 0x15
-    jc .error
-    cmp al,0x0
-    je .enable_a20
+    ; This function need to be written by you.
+    mov ax,0x2402   ;intitiating the function 2402 to be stored in ax
+    int 0x15        ; intitiating the int 15 to check a20 gate with the corresponding fn number
+    jc .error       ; checking the carry flag and jumping to the error label 
+    cmp al,0x0      ;  comparing the al with 0x0 as a result from the int 15 
+    je .enable_a20  ; if equal enable the a20 gate 
 
-    popa                                ; Restore all general purpose registers from the stack
-    ret
+    popa    ;retreive the general purpose registers again from the stack
+    ret     ; return to the call 
