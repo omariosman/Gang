f3428b269e6506365c6b374c9aa9e19328eec00b
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 17:22:31 +0200

    Update bios_cls.asm

--- sources/includes/first_stage/bios_cls.asm
+++ sources/includes/first_stage/bios_cls.asm
@@ -1,9 +1,9 @@
-;************************************** bios_cls.asm **************************************      
-bios_cls:   ; A routine to initialize video mode 80x25 which also clears the screen
-	bios_cls: ; A routine to initialize video mode 80x25 which also clears the screen
-	pusha ; Save all general purpose registers on the stack
-	mov ah,0x0 ; Set Video Mode Function
-	mov al,0x3 ; 80x25 16 color text mode
-	int 0x10 ; Issue INT 0x10
-	popa ; Restore all general purpose registers from the stack
-	ret
+;************************************** bios_cls.asm **************************************
+      bios_cls:   ; A routine to initialize video mode 80x25 which also clears the screen
+
+        pusha           ; push all registers used for general purposes to the stack
+        mov ah,0x0      ; Set ah to zero indicating the function for video mode
+        mov al,0x3      ; set al to 3 refering to the 80x25 16 color text mode
+        int 0x10        ; perform system call to intialize video mode 80x25
+        popa            ; pop all general purpose registers back from the stack and restore them
+        ret             ; Set PC register to return address after poping it from the stack
