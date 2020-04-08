0ae736eb63b0301d8bfcc1fbfb661eae95c976b9
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 17:24:51 +0200

    Update bios_print_hexa.asm

--- sources/includes/first_stage/bios_print_hexa.asm
+++ sources/includes/first_stage/bios_print_hexa.asm
@@ -5,8 +5,8 @@
             .loop:                                    ; Loop on all 4 digits
                   mov esi,ebx                           ; Move current bx into si
                   and esi,0xF0000000                       ; Extract the first left most hexa digits (4 bits)
-                  shr esi,0x1c                          ; Shift SI 12 bits right 
-                  mov al,[hexa_digits+esi]             ; get the right hexadcimal digit from the array           
+                  shr esi,0x1c                          ; Shift SI 12 bits right
+                  mov al,[hexa_digits+esi]             ; get the right hexadcimal digit from the array
                   mov ah, 0x0E                        ; INT 0x10 print character function
                   int 0x10                            ; Print character
                   shl ebx,0x4                          ; Shift bx 4 bits left so the next digits is in the right place to be processed
@@ -20,7 +20,7 @@
       bios_print_hexa_with_prefix:  ; A routine to print a 16-bit value stored in di in hexa decimal (4 hexa digits)
             pusha                                     ; Save all general purpose registers on the stack
             mov si,hexa_prefix                        ; Print the hexadecimal prefix "0x"
-            call bios_print                                 
+            call bios_print
             call bios_print_hexa
             popa                                      ; Restore all general purpose registers from the stack
             ret
