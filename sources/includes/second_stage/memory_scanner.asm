d3309272e88d029c6d6a74b4490ae3dfe1e399d3
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 18:07:42 +0200

    Update memory_scanner.asm

--- sources/includes/second_stage/memory_scanner.asm
+++ sources/includes/second_stage/memory_scanner.asm
@@ -3,38 +3,42 @@
 %define PTR_MEM_REGIONS_TABLE       0x1018
 %define MEM_MAGIC_NUMBER            0x0534D4150                
     memory_scanner:
-            pusha                                       ; Save all general purpose registers on the stack
-                  ; This function need to be written by you.
-            pusha ; Save all general purpose registers on the stack
-            mov ax,MEM_REGIONS_SEGMENT ; Set ES to 0x2000
-            mov es,ax
-            xor ebx,ebx ; Set EBX to zero
-            mov [es:PTR_MEM_REGIONS_COUNT],word 0x0 ; Use the word at address 0x2000:0x0000 as a counter to count memory regions
-            mov di, PTR_MEM_REGIONS_TABLE ; Set DI to point to address 0x18 to be 24-bytes aligned to store the memory regions table
-            .memory_scanner_loop: ; Loop over available memory regions
-            mov edx,MEM_MAGIC_NUMBER ; Set DX to magic number 0x0534D4150 = 'SMAP'
-            mov word [es:di+20], 0x1 ; This is needed by function 0xe820 int 0x15
-            mov eax, 0xE820 ; Set the memory scanner function number
-            mov ecx,0x18 ; Set size of the memory buffer to store region data
-            int 0x15
-            jc .memory_scan_failed ; If carry flag thens something wrong happened so we need to exit with error message
-            cmp eax,MEM_MAGIC_NUMBER ; If eax is equal to the magic number then everything is okay
-            jnz .memory_scan_failed ; Else something wrong happened so we need to exit with error message
-            add di,0x18 ; Advance di 24 bytes to point to the next entry in the memory region table
-            inc word [es:PTR_MEM_REGIONS_COUNT] ; Increment the memory regions counter
-            cmp ebx,0x0 ; If ebx is 0x0 then no more regions and we need to exit the loop
-            jne .memory_scanner_loop ; Else loop to fetch next region
-            jmp .finish_memory_scan ; If we are here then everything went well so we need to skip the error section
+            pusha                                       ;store all general purpose registers in the stack
+            
+            ; This function need to be written by you.
+            pusha                       ;store all general purpose registers in the stack
+            mov ax,MEM_REGIONS_SEGMENT  ;get the segment 2000 in ax
+            mov es,ax                   ;get the copy in es
+            xor ebx,ebx                 ;reset ebx to zero
+            mov [es:PTR_MEM_REGIONS_COUNT],word 0x0         ;get the word at the es:2000 as a counter and initialize it with zero
+            mov di, PTR_MEM_REGIONS_TABLE                   ;let di point to address 18 and make it 24 byte word aligned
+            
+            .memory_scanner_loop:                           
+            mov edx,MEM_MAGIC_NUMBER            ;makeing dx set to the magic number SMAP
+            mov word [es:di+20], 0x1            ;preparation for function e820 and interrupt 15
+            mov eax, 0xE820                     ;move the function numner into eax
+            mov ecx,0x18                        ;ecx has the memory size to be surfed through
+            int 0x15                            ;inititate interrupt 15
+            
+            jc .memory_scan_failed              ;check carry falg, if set then then the memory scanning has failed
+            cmp eax,MEM_MAGIC_NUMBER            ;compare the eax with the magic number
+            jnz .memory_scan_failed             ;if not equal to zero then the memory scan has failed
+            add di,0x18                         ;increment the di pointer by 28 bytes to move on with the next partition of the memory
+            inc word [es:PTR_MEM_REGIONS_COUNT] ;increment the counter of memoery region
+            cmp ebx,0x0                         ;compare ebx with zero
+            jne .memory_scanner_loop            ;if not equal then loop again over the memory scanner
+            jmp .finish_memory_scan             ;else memory scan has finished
+            
             .memory_scan_failed:
             …………..
             .finish_memory_scan:
 
-            popa                                        ; Restore all general purpose registers from the stack
-            ret
+            popa                                ;restore the general purpose registors from the stack
+            ret                                 ;return to the original call
 
     print_memory_regions:
             pusha
-            mov ax,MEM_REGIONS_SEGMENT                  ; Set ES to 0x0000
+            mov ax,MEM_REGIONS_SEGMENT                  
             mov es,ax       
             xor edi,edi
             mov di,word [es:PTR_MEM_REGIONS_COUNT]
