3dfa16e5c66abd1409c07502079d002e520c0c39
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 17:55:24 +0200

    Update pic.asm

--- sources/includes/second_stage/pic.asm
+++ sources/includes/second_stage/pic.asm
@@ -4,14 +4,14 @@
 %define SLAVE_PIC_DATA_PORT         0xA1
 
 
-    disable_pic:
-        pusha
-        mov al,0xFF
-        out MASTER_PIC_DATA_PORT,al
-        out SLAVE_PIC_DATA_PORT,al
-        nop
-        nop
-        mov si, pic_disabled_msg
-        call bios_print
-        popa
-        ret
+    ;disable_pic:
+        ;pusha
+        ;mov al,0xFF
+        ;out MASTER_PIC_DATA_PORT,al
+        ;out SLAVE_PIC_DATA_PORT,al
+        ;nop
+        ;nop
+        ;mov si, pic_disabled_msg
+        ;call bios_print
+        ;popa
+        ;ret
