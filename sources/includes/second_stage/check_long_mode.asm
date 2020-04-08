c0403aa7915e6dcf5dc82ff753fd325421ce0e65
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-04-08 17:53:07 +0200

    Update check_long_mode.asm

--- sources/includes/second_stage/check_long_mode.asm
+++ sources/includes/second_stage/check_long_mode.asm
@@ -1,61 +1,73 @@
         check_long_mode:
-            pusha                           ; Save all general purpose registers on the stack
-            call check_cpuid_support        ; Check if cpuid instruction is supported by the CPU
-            call check_long_mode_with_cpuid ; check long mode using cpuid
-            popa                            ; Restore all general purpose registers from the stack
-            ret
+            pusha                           ;store all general puspose registors in the stack again
+            call check_cpuid_support        ; call this label to check if the CPU supports cpuid
+            call check_long_mode_with_cpuid ; call this label to check if it supports long mode  as well
+            popa                            ;retreive all general pupose registers from the stack 
+            ret                             ; return to the call 
 
         check_cpuid_support:
-            pusha               ; Save all general purpose registers on the stack
-                  ; This function need to be written by you.
-                pushfd ; Push eflags for restoring them at the end of the subroutine
-                pushfd ; Push eflags again to use them for comparison
-                pushfd ; Copy flags to eax
-                pop eax
-                xor eax,0x0200000 ; Flip bit 21
-                push eax ; Copy value of eax to the eflags
-                popfd
-                pushfd ; Copy eflags to eax
-                pop eax
-                pop ecx ; Copy original eflags to ecx (second pushfd above)
+            pusha               ;store all general puspose registors in the stack again
+            
+                ; This function need to be written by you.
+                pushfd                          ;store the eflags in the stack 
+                pushfd                          ;store them again for comparison purposes
+                pushfd                          ;store them again for storage purposes
+                pop eax                         ;store the eflags in eax
+                
+                xor eax,0x0200000               ;this xor is used to flip bit 21, however this step could be neglected
+                push eax                        ;store the xored value of eflags in the stack again
+                popfd                           ;retreive them again from the stack
+                pushfd                          ;store them again
+                pop eax                         ;retreive the eflags into eax again 
+                
+                pop ecx                         ;retrieve the original eflags into ecx for comparison purposes
                 ; Now if the modified bit 21 is written to the eflags then bit 21 in eax is different from that in eax
-                xor eax,ecx ; This means that xoring the two bits will always produce 1
-                and eax,0x0200000 ; Zero out all bits except bit 21, it will stay as is
-                cmp eax,0x0 ; If eax equal zero this means that bit 21 was not modified in eflags
-                jne .cpuid_supported ; Jump to .cpuid_supported if cpuid is supported
-                mov si,cpuid_not_supported ; Else print error message and hang as we cannot proceed
-                call bios_print
-                jmp hang
-                .cpuid_supported: ; Print a message indicating that cpuid is supported
-                mov si,cpuid_supported
-                call bios_print
+                xor eax,ecx                     ;xor the eax and ecx to check/see the converted 21st bit
+                and eax,0x0200000               ;reset all bits except bit 21 
+                cmp eax,0x0                     ;check the result of the and check if the bit 21 is zero or not
+                jne .cpuid_supported            ;if not equal to zero then it supports cpuid
+                mov si,cpuid_not_supported      ;otherwise it does not support cpuid
+                call bios_print                 ;call this label to print the cpuid is not supported (stored in si)
+                jmp hang                        ;hang the process
+                
+                .cpuid_supported:               ;label called to print cpuid is supported
+                mov si,cpuid_supported          
+                call bios_print                 ;print what is in the si (cpuid supported)
                 popfd
 
 
 
-            popa                ; Restore all general purpose registers from the stack
-            ret
+            popa                ; retreive all general pupose registers from the stack 
+            ret                 ; return to the call 
+            
+            
+            
 
         check_long_mode_with_cpuid:
-            pusha                                   ; Save all general purpose registers on the stack
-                  ; This function need to be written by you.
-                mov eax,0x80000000 ; cpuid function to determine the largest function number
+            pusha                                ;store all general puspose registors in the stack again
+            
+            
+                ; This function need to be written by you.
+                mov eax,0x80000000              ;function 8 milion to check the long mode in cpuid
                 cpuid
-                cmp eax,0x80000001 ; If the largest function number is less than 0x80000001 then it is not supported
-                jl .long_mode_not_supported ; Error and hang
-                mov eax,0x80000001 ; Else invoke cpuid function 0x80000001 to get the processor extended features bits (EDX).
+                cmp eax,0x80000001              ;check if the largest function number is bigger than 8 mil and one then it is not supported
+                jl .long_mode_not_supported     ;if so go to long mode not supported
+                mov eax,0x80000001              ;otherwise get this function invoked
                 cpuid
-                and edx,0x20000000 ; Mask out all bits in edx except bit # 29 which is the Long Mode LM-bit
-                cmp edx,0 ; if edx is zero them LM-bit is not set
-                je .long_mode_not_supported ; If Long mode is not supported then we need to print a message and stop
-                mov si,long_mode_supported_msg ; Else print a message indicating the long mode is supported
-                call bios_print
-                jmp .exit_check_long_mode_with_cpuid ; Skip over the long mode not supported section
+                and edx,0x20000000              ;reset all bits except bit 29 for long mode purposes
+                cmp edx,0                       ;compare the result of the and with zero
+                je .long_mode_not_supported     ;if the result is equal to zero then long mode is not supported
+                mov si,long_mode_supported_msg  ;store the long mode supported message in si for printing
+                call bios_print                 ;print what is in si
+                
+                jmp .exit_check_long_mode_with_cpuid    ;otherwise call long mode with cpuid
                 .long_mode_not_supported:
-                mov si,long_mode_not_supported_msg ; If we are here then long mode is not supported
-                call bios_print ; Print an error message and jump to hang
-                jmp hang
+                mov si,long_mode_not_supported_msg      ;store the long mode not supported message in si for printing
+                call bios_print                          ;print what is in si
+                jmp hang                                ;then hang the process
+                
                 .exit_check_long_mode_with_cpuid:
 
-            popa                                ; Restore all general purpose registers from the stack
-            ret
+            popa                ; retreive all general pupose registers from the stack 
+            ret                 ; return to the call 
+            
